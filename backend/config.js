1a5952095af48bde1d88b752df40aa04ae41ca57
Anuj Kundar <anujkundar003@gmail.com>
2023-11-22 00:12:50 +0530

    add book model and test using post method

--- backend/config.js
+++
@@ -1,2 +0,0 @@
-export const PORT =5555;
-export const mongoDBURL='mongodb+srv://root:root@library.pbnu4ts.mongodb.net/?retryWrites=true&w=majority'
--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,51 @@
-import express from "express";
-import {PORT,mongoDBURL} from "./config.js";
+import express, { request } from "express";
+import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 
 const app = express();
 
+// middleware
+app.use(express.json());
 // testing routes
-app.get('/',(request,response)=>{
+app.get('/', (request, response) => {
     console.log(request);
     return response.status(234).send('Testing Routes')
 })
 
+app.post('/books', async (request, response) => {
+    try {
+        if (!request.body.title || 
+            !request.body.author || 
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: 'Please fill all required fields',
+            });
+        }
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear,
+        };
+        const book = await Book.create(newBook);
+        return response.status(201).send(book);
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+
+    }
+});
+
 
 mongoose
     .connect(mongoDBURL)
-    .then(()=>{
+    .then(() => {
         console.log('Connected to MongoDB');
-        app.listen(PORT,()=>{
+        app.listen(PORT, () => {
             console.log(`App is Listening to Port: ${PORT}`)
         });
     })
-    .catch((error)=>{
+    .catch((error) => {
         console.log(error)
     })
