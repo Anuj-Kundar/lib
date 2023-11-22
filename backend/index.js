c9fd6438464bdf112b56419d3bb383da58f15633
Anuj Kundar <anujkundar003@gmail.com>
2023-11-22 20:33:56 +0530

    add middleware for dealing with cors policy error

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,22 @@ import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
 import booksRoutes from "./routes/booksRoute.js";
+import cors from 'cors';
 
 const app = express();
 
-// middleware
+// middleware for parsing request body
 app.use(express.json());
+
+// middleware for cors errors
+app.use(
+    cors({
+        origin:'http://localhost:3000',
+        methods:['GET','POST','PUT','DELETE'],
+        allowedHeaders:['Content-Type'],
+    })
+);
+
 // testing routes
 app.get('/', (request, response) => {
     console.log(request);
